470f04c9c980a66aac6d8ca9259942b84b6a7c36
python <eglee04302gmail.com>
2024-03-19 19:22:05 +0900

    v1.1 prime_number

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,20 +1,19 @@
-number = int(input("Input number: "))
+start = int(input("Input start number: "))
+end = int(input("Input end number: "))
 
-is_prime_number = True #Change variable name from count to is_prime_number for readability.gid
-if number <2:
-    is_prime_number = False
-else:
-    # for i in range(2, number):
-    i = 2
-    while i*i <= number: # bug fix
-        if number % i == 0:
-            is_prime_number = False #Remove addition operation
-            break #Exit the loop when the first divisor is found. Performance is improved when the input value is not a prime number.
-        # print(i, end=" ")
-        i = i+1
+for k in range(start, end+1):
+    is_prime_number = True
+    if k < 2:
+        is_prime_number = False
+    else:
+        i = 2
+        while i * i <= k:
+            if k % i == 0:
+                is_prime_number = False
+                break
+            i = i + 1
+        if is_prime_number: print(k, end=' ')
 
-#if is_prime_number ==0:
-if is_prime_number: #Remove comparison (equivalent) operators
-    print(f"{number} is prime number~")
-else:
-    print(f"{number} is NOT prime number!")
+
+
+
