1f13b5183b70019bc0d8c095d594f9f9d42fe9b0
python <eglee04302gmail.com>
2024-03-19 20:55:57 +0900

    v1.5 prime number

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -4,13 +4,24 @@ def is_prime_number(n) -> bool:
     :param n: positive integer
     :return: Returns True if it is a prime number, and returns False if it is not a prime number.
     """
-    pass
+    #is_prime_number = True
+    if k<2:
+        #is_prime_number = False
+        return False
+    else:
+        i=2
+        while i * i <= k:
+            if k%i == 0:
+                #is_prime_number = False
+                #break
+                return False
+            i = i+1
+        return True
 
-start, end = list(map(int, input("Input start number: ").split()))
+start, end = list(map(int, input("Input start & end number: ").split()))
 
 for k in range(start, end+1):
-
-        if is_prime_number: print(k, end=' ')
+    if is_prime_number(k): print(k, end=' ')
 
 
 
