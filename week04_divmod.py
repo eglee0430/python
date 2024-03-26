75c95c7556d080a426be58971d7c29fe47c4b7f0
python <eglee04302gmail.com>
2024-03-26 21:24:56 +0900

    v0.2 math

--- week04_divmod.py
+++ week04_divmod.py
@@ -1,8 +1,18 @@
-dividend, divisor = map(int, input("Input dividend & divisor number: ").split())
-print(f"{dividend}//{divisor} = {dividend // divisor}")
-print(f"{dividend}//{divisor} = {dividend % divisor}")
-print(f"{dividend}//{divisor} = {divmod(dividend,divisor)[0]}")
-print(f"{dividend}//{divisor} = {divmod(dividend,divisor)[1]}")
+def print_fx(fx):
+    texts = "f(x) = "
+    expo = len(fx) - 1
 
+    for i in range(len(fx)):
+        coef = fx[i]
+        if coef == 0:
+            expo = expo - 1
+            continue
+        if coef>=0 and i!=0:
+            texts=texts + "+"
+        texts= texts + str(coef) + "x^" + str(expo)
+        expo = expo - 1
+    return texts
 
-print(divmod(dividend,divisor))
+coefficient = [5,-2,0,6]
+
+print(print_fx(coefficient))
