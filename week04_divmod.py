0bb73983c559844c750ed207beb815fbac33c456
python <eglee04302gmail.com>
2024-03-26 19:44:27 +0900

    v0.1 divmod

---
+++ week04_divmod.py
@@ -0,0 +1,8 @@
+dividend, divisor = map(int, input("Input dividend & divisor number: ").split())
+print(f"{dividend}//{divisor} = {dividend // divisor}")
+print(f"{dividend}//{divisor} = {dividend % divisor}")
+print(f"{dividend}//{divisor} = {divmod(dividend,divisor)[0]}")
+print(f"{dividend}//{divisor} = {divmod(dividend,divisor)[1]}")
+
+
+print(divmod(dividend,divisor))
--- week04_power.py
+++ week04_power.py
@@ -11,6 +11,7 @@ def power(b, e) -> int:
     return result
 
 
+
 base, exponent = map(int, input("Input base & exponent number: ").split())
 print(f"{base}^{exponent} = {base**exponent}") #operator
 print(f"{base}^{exponent} = {pow(base, exponent)}") #built-in function
